7b2f935a7cd124af76fe9bc2b8b8ee30a591a5f6
Elen <15el.shev@gmail.com>
2022-05-30 20:37:21 +0300

    Add formatFIO()

--- index.js
+++ index.js
@@ -1,40 +1,55 @@
 "use strict";
 
 class User {
-  constructor(name, surname) {
+  constructor(name, surname, year) {
     this.name = name;
     this.surname = surname;
     this.year = year;
   }
   getFullName() {
-    return 0;
+    return `${this.name} ${this.surname}`;
   }
 }
 class Student extends User {
-  constructor(radius) {
-    super("Ball");
-    this.radius = radius;
+  constructor(name, surname, year) {
+    super(name, surname, year);
   }
   getCourse() {
-    return 0;
+    return new Date().getFullYear() - this.year;
+  }
+  formatFIO() {
+    console.log(`${this.surname} ${this.surname.charAt(0)}.`);
   }
 }
 
 class Group {
-  constructor(name) {
+  constructor(name, students) {
     this.name = name;
-    this.students = [];
-    this.year = year;
+    this.students = students;
   }
   showStudents() {
-    return 0;
+    if (this.students.length > 0) {
+      return this.students.map(function (element) {
+        return element.formatFIO();
+      });
+    }
+    throw new RangeError("Passed an empty array.");
   }
 }
 
-// const ball = new Ball(-49);
-// const cylinder = new Cylinder(15, 18);
-// const cube = new Cube(56);
+const user = new User("Elon", "Musk", 2015);
+const student = new Student("Elon", "Musk", 2015);
+const group = new Group("Main group", [
+  new Student("Elon", "Musk", 2015),
+  new Student("Alfred", "Nobel", 2005),
+  new Student("Oscar", "Wilde", 2020),
+  new Student("Rudyard ", "Kipling", 2014),
+  new Student("Dali", "Salvador", 2008),
+  new Student("Sigmund", "Freud ", 2021),
+  new Student("Princess", "Diana", 2022),
+  new Student("Jack", "London", 2023),
+]);
 
-// console.log(getVolumeAnyFigura(ball));
-// console.log(getVolumeAnyFigura(cylinder));
-// console.log(getVolumeAnyFigura(cube));
+console.log(user.getFullName());
+console.log(student.getCourse(user));
+console.log(group.showStudents());
