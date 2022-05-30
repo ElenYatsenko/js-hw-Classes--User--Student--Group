f965b3d4c6d07636f8a276260f940481972643d8
Elen <15el.shev@gmail.com>
2022-05-30 21:54:00 +0300

    Added check for a year

--- index.js
+++ index.js
@@ -6,6 +6,20 @@ class User {
     this.surname = surname;
     this.year = year;
   }
+  get year() {
+    return this._year;
+  }
+  set year(year) {
+    if (year > new Date().getFullYear()) {
+      throw new RangeError(
+        "The year value must be greater than the current year."
+      );
+    }
+    if (year < new Date().getFullYear() - 5) {
+      throw new RangeError("Year value must not exceed 5 years.");
+    }
+    this._year = year;
+  }
   getFullName() {
     return `${this.name} ${this.surname}`;
   }
@@ -18,7 +32,7 @@ class Student extends User {
     return new Date().getFullYear() - this.year;
   }
   formatFIO() {
-    console.log(`${this.surname} ${this.surname.charAt(0)}.`);
+    return `${this.surname} ${this.surname.charAt(0)}.`;
   }
 }
 
@@ -29,25 +43,27 @@ class Group {
   }
   showStudents() {
     if (this.students.length > 0) {
-      return this.students.map(function (element) {
-        return element.formatFIO();
-      });
+      return this.students
+        .map(function (element) {
+          return element.formatFIO();
+        })
+        .join(" ");
     }
     throw new RangeError("Passed an empty array.");
   }
 }
 
-const user = new User("Elon", "Musk", 2015);
-const student = new Student("Elon", "Musk", 2015);
+const user = new User("Elon", "Musk", 2017);
+const student = new Student("Elon", "Musk", 2019);
 const group = new Group("Main group", [
-  new Student("Elon", "Musk", 2015),
-  new Student("Alfred", "Nobel", 2005),
+  new Student("Elon", "Musk", 2018),
+  new Student("Alfred", "Nobel", 2020),
   new Student("Oscar", "Wilde", 2020),
-  new Student("Rudyard ", "Kipling", 2014),
-  new Student("Dali", "Salvador", 2008),
+  new Student("Rudyard ", "Kipling", 2018),
+  new Student("Dali", "Salvador", 2018),
   new Student("Sigmund", "Freud ", 2021),
   new Student("Princess", "Diana", 2022),
-  new Student("Jack", "London", 2023),
+  new Student("Jack", "London", 2020),
 ]);
 
 console.log(user.getFullName());
